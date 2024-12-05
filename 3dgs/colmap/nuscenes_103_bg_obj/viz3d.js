ca6e43f8ff4e897455cd78de8f101a22b7f02990
Arun Balajee Vasudevan <arunbalv@andrew.cmu.edu>
2024-12-05 00:35:08 -0500

    Update viz3d.js

--- 3dgs/colmap/nuscenes_103_bg_obj/viz3d.js
+++ 3dgs/colmap/nuscenes_103_bg_obj/viz3d.js
@@ -140,8 +140,8 @@ function init() {
 
     // perspective camera
     // perspectiveCamera.position.x = 0;
-    perspectiveCamera.position.y = -5;
-    perspectiveCamera.position.z = 20;
+    perspectiveCamera.position.y = -20;
+    perspectiveCamera.position.z = 10;
     perspectiveCamera.up.set(0, 0, 1); perspectiveCamera.lookAt(0, 0, -1);
 
     // Load point cloud
